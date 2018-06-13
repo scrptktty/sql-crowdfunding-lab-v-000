51d90e1adbfa5cf462ad516b60c97271cd59f85c
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 02:34:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,8 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT name, age, SUM(pledges.amount) FROM users
   JOIN pledges ON pledges.user_id = users.id
-  GROUP BY pledges.category
+  INNER JOIN projects ON pledges.project_id = projects.id
+  GROUP BY projects.category
   ORDER BY name"
 end
 
