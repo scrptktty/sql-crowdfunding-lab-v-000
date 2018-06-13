cdd335be3dfb964511164c38045a8a48cf8a2064
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 02:32:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,8 +28,8 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT name, age, SUM(pledges.amount) FROM users
   JOIN pledges ON pledges.user_id = users.id
-  ORDER BY pledges.category
-  GROUP BY name"
+  GROUP BY pledges.category
+  ORDER BY name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
