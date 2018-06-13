9a78c267a6b4913eeb4bed9fc6a208a43d721226
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 02:40:17 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,10 +26,11 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT name, SUM(pledges.amount) FROM users
+  "SELECT name, SUM(pledges.amount) as pledge_total FROM users
   JOIN pledges ON pledges.user_id = users.id
   INNER JOIN projects ON pledges.project_id = projects.id
-  GROUP BY name"
+  GROUP BY name
+  ORDER BY name ASC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
