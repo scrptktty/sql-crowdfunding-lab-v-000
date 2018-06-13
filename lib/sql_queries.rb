3ced303b9cbb0e8e4d097349d883b850b37a4124
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 18:37:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,3 +1,4 @@
+require "pry"
 # Write your sql queries in this file in the appropriate method like the example below:
 #
 # def select_category_from_projects
@@ -5,8 +6,6 @@
 # end
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
-
-require "pry"
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT title, SUM(pledges.amount) AS project_total FROM projects
