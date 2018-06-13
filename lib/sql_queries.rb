3ce97c4a46a6d50a0d411e7d5145680af3bb9572
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 02:21:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT title, pledges.amount - funding_goal FROM projects
+  "SELECT title, SUM(pledges.amount) - funding_goal FROM projects
   JOIN pledges ON pledges.project_id = projects.id
   WHERE pledges.amount >= funding_goal"
 end
