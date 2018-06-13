e302f84af5a13b957a1093d9a85cc1c01e1bd207
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 17:58:53 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,9 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   # INNER JOIN pledges ON users.id = pledges.user_id
   # WHERE category='music'"
 
-  "SELECT * FROM projects
+  "SELECT category, SUM(pledges.amount) FROM projects
+  JOIN pledges ON pledges.project_id = projects.id
+  GROUP BY category
   WHERE category = 'music'"
 end
 
