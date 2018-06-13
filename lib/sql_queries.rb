790e1c4e1072b85abd4075c77a9f15267438b0e6
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 02:24:59 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,8 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT title, (SUM(pledges.amount) - funding_goal) as total_over FROM projects
   JOIN pledges ON pledges.project_id = projects.id
-  WHERE total_over > 0"
+  WHERE total_over > 0
+  GROUP BY title"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
